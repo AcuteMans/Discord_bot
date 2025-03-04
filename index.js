2b1b1ffefe19136ed657859edf2be9b35150bbaa
AcuteMans <taoharper18@gmail.com>
2025-03-04 21:51:30 +0200

    feat/receive-message

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// First Commit
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits , Partials} from "discord.js";
+import "dotenv/config";
+
+// Create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+  partials: [Partials.Channel, Partials.Message],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "hello") {
+    message.reply("hello buddy");
+  }
+});
